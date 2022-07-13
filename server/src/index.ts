f2ba5cea6af0ea31f4989ef48cdd4d86b29be57f
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-13 04:34:13 +0200

    Set up express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+const PORT = process.env.SERVER_PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
