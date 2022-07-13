9a13d45b51265f74ec83082f52732f830fbe5550
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-13 04:46:02 +0200

    Add winston logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { logger } from './log';
 
 const app = express();
 
 const PORT = process.env.SERVER_PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on port: ${PORT}`);
+  logger.info(`Listening on port: ${PORT}`);
 });
