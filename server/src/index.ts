855697e5ef67062ee5ecdb9ff6986c51909f7bc8
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-13 04:49:26 +0200

    Set up server config folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,8 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { logger } from './log';
+import { PORT } from './config/constants';
+import { logger } from './config/log';
 
 const app = express();
-
-const PORT = process.env.SERVER_PORT || 4000;
 
 app.listen(PORT, () => {
   logger.info(`Listening on port: ${PORT}`);
