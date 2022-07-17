f5a82fa8294df6952a4ddf055b04d14a10a11390
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-17 05:10:25 +0200

    feat(server): add simple routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,16 @@ import { logger } from './config/log';
 
 const app = express();
 
+app.get('/', (_req: Request, res: Response, _next: NextFunction) => {
+  logger.info('Request on /');
+  res.send({ message: 'Hello from the server' });
+});
+
+app.get('/pieterpad', (_req: Request, res: Response, _next: NextFunction) => {
+  logger.info('Request on /pieterpad');
+  res.send({ message: 'Pieterpad' });
+});
+
 app.listen(PORT, () => {
   logger.info(`Listening on port: ${PORT}`);
 });
