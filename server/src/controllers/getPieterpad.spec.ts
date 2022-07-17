710e6b22d46899a8cb4925a98a08f6698e10ff38
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-17 13:26:19 +0200

    test: getPieterpadController

---
+++ server/src/controllers/getPieterpad.spec.ts
@@ -0,0 +1,43 @@
+import { Request, Response } from 'express';
+import { getPieterpadController } from './getPieterpad';
+import pieterpadData from '../../../data/pieterpad.json';
+jest.mock(
+  '../../../data/pieterpad.json',
+  () => ({
+    foo: 'bar',
+  }),
+  { virtual: true }
+);
+
+const mockRequest = () => {
+  const req = {} as Request;
+  return req;
+};
+
+const mockResponse = () => {
+  const res = {} as Response;
+  res.send = jest.fn().mockReturnValueOnce(res);
+  res.status = jest.fn().mockReturnValueOnce(res);
+  return res;
+};
+
+const mockNext = () => jest.fn();
+
+describe('getPieterpadController', () => {
+  it('should return 200 with data, message and status ', () => {
+    const req = mockRequest();
+    const res = mockResponse();
+    const next = mockNext();
+
+    getPieterpadController(req, res, next);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledTimes(1);
+    expect(res.send).toHaveBeenCalledWith({
+      message: 'data for pieterpad',
+      data: pieterpadData,
+      status: 'succes',
+    });
+    expect(next).not.toHaveBeenCalled();
+  });
+});
--- server/src/controllers/getPieterpad.ts
+++ server/src/controllers/getPieterpad.ts
@@ -2,20 +2,15 @@ import { Request, Response, NextFunction } from 'express';
 import { logger } from '../config/log';
 import pieterpadData from '../../../data/pieterpad.json';
 
-export const getPieterpadController = async (
+export const getPieterpadController = (
   _req: Request,
   res: Response,
   _next: NextFunction
 ) => {
-  try {
-    logger.info('request for pieterpad data');
-    res.status(200).send({
-      message: 'data for pieterpad',
-      data: pieterpadData,
-      status: 'succes',
-    });
-  } catch (error) {
-    logger.error(error);
-    res.status(500).send({ message: 'something went wrong' });
-  }
+  logger.info('request for pieterpad data');
+  res.status(200).send({
+    message: 'data for pieterpad',
+    data: pieterpadData,
+    status: 'succes',
+  });
 };
