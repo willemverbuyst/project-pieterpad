ef5f6b7a93232e4445464af74fd49518e66c9070
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-17 06:19:54 +0200

    feat: hook up morgan

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,12 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { PORT } from './config/constants';
 import { logger } from './config/log';
+import { morganMiddleware } from './config/morgan';
 
 const app = express();
 
 app.use(cors());
+app.use(morganMiddleware);
 
 app.use(express.static(`${__dirname}/public/`));
 
@@ -21,6 +23,11 @@ app.get('/root', (_req: Request, res: Response, _next: NextFunction) => {
 app.get('/pieterpad', (_req: Request, res: Response, _next: NextFunction) => {
   logger.info('Request on /pieterpad');
   res.send({ message: 'Pieterpad' });
+});
+
+app.get('*', (_req: Request, res: Response, _next: NextFunction) => {
+  logger.warn('Bad request');
+  res.send({ message: 'Oops 404, page not found' });
 });
 
 app.listen(PORT, () => {
