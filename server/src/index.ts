706a0248007d2bb2cfe6507befe7fd46e0fc045c
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-17 05:51:13 +0200

    feat: serve public folder

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,19 @@
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 import { PORT } from './config/constants';
 import { logger } from './config/log';
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.static(`${__dirname}/public/`));
+
 app.get('/', (_req: Request, res: Response, _next: NextFunction) => {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+app.get('/root', (_req: Request, res: Response, _next: NextFunction) => {
   logger.info('Request on /');
   res.send({ message: 'Hello from the server' });
 });
