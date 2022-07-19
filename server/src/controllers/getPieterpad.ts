14eed1d97cd8cc9e49a4ff0fae669a90ae7d81f1
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-19 20:16:36 +0200

    feat: wrap in docker environment

--- server/src/controllers/getPieterpad.ts
+++ server/src/controllers/getPieterpad.ts
@@ -1,14 +1,21 @@
 import { Request, Response, NextFunction } from 'express'
-import pieterpadData from '../../../data/pieterpad.json'
+import { Stage } from '../integration/database/models/stageModel'
 
-export const getPieterpadController = (
+export const getPieterpadController = async (
   _req: Request,
   res: Response,
   _next: NextFunction
 ) => {
-  res.status(200).send({
-    message: 'data for pieterpad',
-    data: pieterpadData,
-    status: 'succes',
-  })
+  const stages = await Stage.find()
+  try {
+    res.status(200).send({
+      message: 'data for pieterpad',
+      data: stages,
+      status: 'succes',
+    })
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+    })
+  }
 }
--- server/src/integration/database/mongo.ts
+++ server/src/integration/database/mongo.ts
@@ -1,13 +1,7 @@
 import mongoose from 'mongoose'
 import { logger } from '../../config/log'
-import { Stage } from './models/stageModel'
 
-const MONGO_IP = 'mongo'
-const MONGO_PORT = 27017
-const MONGO_USER = process.env.MONGO_USER || 'myusername'
-const MONGO_PASSWORD = process.env.MONGO_PASSWORD || 'mypassword'
-
-const DB_CONNECTION_STRING = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+const DB_CONNECTION_STRING = `mongodb://mongodb:27017/testdb`
 
 export const connectWithRetry = () => {
   console.log('DB_CONNECTION_STRING ', DB_CONNECTION_STRING)
