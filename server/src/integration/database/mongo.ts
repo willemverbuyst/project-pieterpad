9a780e318226b31f9bf6cf32f7e684d42e5e1e89
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-21 19:15:55 +0200

    feat: use parcel for client

--- server/src/integration/database/mongo.ts
+++ server/src/integration/database/mongo.ts
@@ -4,7 +4,6 @@ import { logger } from '../../config/log'
 const DB_CONNECTION_STRING = `mongodb://mongodb:27017/testdb`
 
 export const connectWithRetry = () => {
-  console.log('DB_CONNECTION_STRING ', DB_CONNECTION_STRING)
   mongoose
     .connect(DB_CONNECTION_STRING)
     .then(() => logger.info('successfully connected to the db'))
