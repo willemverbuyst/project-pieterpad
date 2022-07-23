f45721f6dcd029df65a7d5b4a1593d6a5fdbeb44
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-23 18:41:43 +0200

    chore: clean up

--- scraper/main.py
+++ scraper/main.py
@@ -1,10 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-print("start")
 
-# URL = "https://realpython.github.io/fake-jobs/"
-URL = 'http://localhost:1234'
 page = requests.get("http://172.17.0.1:1234/") 
 
 
