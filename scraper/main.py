e8d01ba6ef784e2fe6f0b2fd5908a001594c3e74
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-23 18:41:14 +0200

    feat: print client html

--- scraper/main.py
+++ scraper/main.py
@@ -1,1 +1,11 @@
-print("hello world")
+from bs4 import BeautifulSoup
+import requests
+
+print("start")
+
+# URL = "https://realpython.github.io/fake-jobs/"
+URL = 'http://localhost:1234'
+page = requests.get("http://172.17.0.1:1234/") 
+
+
+print(page.text)
