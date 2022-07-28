e082cffa8eaf31c75cc1c3eb9f9704b41d00965c
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-28 05:19:03 +0200

    style: update display

--- chat/public/joinRoom.js
+++ chat/public/joinRoom.js
@@ -1,6 +1,7 @@
 function joinRoom(roomName) {
   namespaceSocket.emit('joinRoom', roomName, (newNumberOfUsers) => {
     document.querySelector('.current-room__title').innerHTML = roomName
-    document.querySelector('.current-room__users').innerHTML = newNumberOfUsers
+    document.querySelector('.container__chat--users').innerHTML =
+      `<span class="user">&#128513;</span>`.repeat(newNumberOfUsers)
   })
 }
