88ef95617d1f3c5aa17817249f7451bf71625d69
Willem Verbuyst <willemverbuyst@gmail.com>
2022-07-28 06:25:29 +0200

    feat: display history

--- public/joinRoom.js
+++ public/joinRoom.js
@@ -4,4 +4,17 @@ function joinRoom(roomName) {
     document.querySelector('.container__chat--users').innerHTML =
       `<span class="user">&#128513;</span>`.repeat(newNumberOfUsers)
   })
+
+  namespaceSocket.on('historyCatchUp', (history) => {
+    const messageList = document.querySelector('.conversation-list')
+    messageList.innerHTML = ''
+
+    history.forEach((message) => {
+      const messageHTML = buildMessageHTML(message)
+      const currentMessages = messageList.innerHTML
+      messageList.innerHTML = currentMessages + messageHTML
+    })
+
+    messageList.scrollTo(0, messageList.scrollHeight)
+  })
 }
